fad37f91a39e1dc7c9f74387664269e578a8e5ee
Amir <amirbehvandi747@gmail.com>
2025-08-01 10:54:13 +0330

    yodate settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -21,7 +21,11 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = [
+    "https://haircut-scheduler.onrender.com",
+    "http://localhost",
+    "http://127.0.0.1",
+]
 
 
 # Application definition
